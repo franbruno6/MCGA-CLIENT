0069aef11a574c160c2c3b3cbf7f0d6a22d7ad65
Francisco Bruno <franbruno99@gmail.com>
2023-12-19 14:37:44 -0300

    style changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { GymClass as GymClassModel } from './models/gymclass';
 import GymClass from './components/GymClass';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button, Spinner } from 'react-bootstrap';
 import styles from './styles/GymClassesPage.module.css';
 import stylesUtils from './styles/utils.module.css';
 import * as GymClassesApi from './network/gymclasses_api';
@@ -12,18 +12,24 @@ import AddEditGymClassDialog from './components/AddEditGymClassDialog';
 
 function App() {
     const [gymClasses, setGymClasses] = useState<GymClassModel[]>([]);
+    const [gymClassesLoading, setGymClassesLoading] = useState(true);
+    const [showGymClassesLoadingError, setShowGymClassesLoadingError] = useState(false);
 
-    const [showAddGymClassDialog, setShowEditAddGymClassDialog] = useState(false);
+    const [showAddGymClassDialog, setShowAddGymClassDialog] = useState(false);
     const [gymClassToEdit, setGymClassToEdit] = useState<GymClassModel | null>(null);
 
     useEffect(() => {
         async function loadGymClasses() {
             try {
+                setShowGymClassesLoadingError(false);
+                setGymClassesLoading(true);
                 const gymClasses = await GymClassesApi.fetchGymClasses();
                 setGymClasses(gymClasses);
             } catch (error) {
                 console.error(error);
-                alert(error);
+                setShowGymClassesLoadingError(true);
+            } finally {
+                setGymClassesLoading(false);
             }
         }
         loadGymClasses();
@@ -38,45 +44,57 @@ function App() {
         }
     }
 
+    const gymClassesGrid =
+    <Row xs={1} md={2} lg={3} className={`g-4 ${styles.gymClassesGrid}`}>
+        {gymClasses.map(gymClass => (
+            <Col key={gymClass._id}>
+                <GymClass
+                    gymClass={gymClass}
+                    className={styles.gymClass}
+                    onGymClassClicked={setGymClassToEdit}
+                    onDeleteGymClassClicked={deleteGymClass}
+                />
+            </Col>
+        ))}
+    </Row>
+
     return (
-        <Container>
+        <Container className={styles.gymClassesPage}>
             <Button
                 className={`mb-4 ${stylesUtils.blockCenter} ${stylesUtils.flexCenter}`}
-                onClick={() => setShowEditAddGymClassDialog(true)}>
+                onClick={() => setShowAddGymClassDialog(true)}>
                 <FaPlus />
                 Add Gym Class
             </Button>
-            <Row xs={1} md={2} lg={3} className="g-4">
-                {gymClasses.map(gymClass => (
-                    <Col key={gymClass._id}>
-                        <GymClass
-                        gymClass={gymClass}
-                        className={styles.gymClass}
-                        onGymClassClicked={setGymClassToEdit}
-                        onDeleteGymClassClicked={deleteGymClass}
-                        />
-                    </Col>
-                ))}
-            </Row>
+            {gymClassesLoading && <Spinner animation='border' variant='primary' />}
+            {showGymClassesLoadingError && <p>Error loading gym classes, please refresh the page</p>}
+            {!gymClassesLoading && !showGymClassesLoadingError &&
+            <>
+                { gymClasses.length > 0
+                    ? gymClassesGrid
+                    : <p className={stylesUtils.blockCenter}>No gym classes found</p>
+                }
+            </>
+            }
             {
                 showAddGymClassDialog && 
                     <ShowAddEditGymClassDialog
-                    onDismiss={() => setShowEditAddGymClassDialog(false)}
-                    onGymClassSaved={(newGymClass) => {
-                        setGymClasses([...gymClasses, newGymClass]);
-                        setShowEditAddGymClassDialog(false);
-                    }}
+                        onDismiss={() => setShowAddGymClassDialog(false)}
+                        onGymClassSaved={(newGymClass) => {
+                            setGymClasses([...gymClasses, newGymClass]);
+                            setShowAddGymClassDialog(false);
+                        }}
                     />
             }
             {
                 gymClassToEdit &&
                 <AddEditGymClassDialog
-                gymClassToEdit={gymClassToEdit}
-                onDismiss={() => setGymClassToEdit(null)}
-                onGymClassSaved={(updatedGymClass) => {
-                    setGymClasses(gymClasses.map(existingGymClass => existingGymClass._id === updatedGymClass._id ? updatedGymClass : existingGymClass));
-                    setGymClassToEdit(null);
-                }}
+                    gymClassToEdit={gymClassToEdit}
+                    onDismiss={() => setGymClassToEdit(null)}
+                    onGymClassSaved={(updatedGymClass) => {
+                        setGymClasses(gymClasses.map(existingGymClass => existingGymClass._id === updatedGymClass._id ? updatedGymClass : existingGymClass));
+                        setGymClassToEdit(null);
+                    }}
                 />
             }
         </Container>
